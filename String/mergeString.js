0f1945bd0607518f0bd9104abf594d260a47e01c
Satyam Agrahari <satyamagrahari952@gmail.com>
2024-07-07 15:46:47 +0530

    added string question merge two string

---
+++ String/mergeString.js
@@ -0,0 +1,18 @@
+// Merge two string
+let word1 = "abc", word2 = "pqr";
+const mergeString = (word1, word2) => {
+    let i = 0, j = 0, finalString = '';
+    while (i < word1.length && j < word2.length) {
+        if (i < word1.length) {
+            finalString += word1.charAt(i);
+            i = i + 1;
+        }
+        if (j < word2.length) {
+            finalString += word2.charAt(j);
+            j = j + 1;
+        }
+    }
+    return finalString;
+}
+
+console.log(mergeString(word1, word2));
