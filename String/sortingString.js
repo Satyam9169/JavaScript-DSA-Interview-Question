d41ba1b49fe41f511051a548119001e8bd055cf0
Satyam Agrahari <38384042+Satyam9169@users.noreply.github.com>
2024-07-08 16:44:43 +0530

    Update sortingString.js

--- String/sortingString.js
+++ String/sortingString.js
@@ -7,6 +7,13 @@ const sortString = (arr) => {
     for (let i = 0; i < n; i++) {
         let char = arr.charCodeAt(i);
         count[char - 97]++;  // here we are counting the frequency of string
+                             // The number 97 is the ASCII value of the lowercase letter 'a'. 
+                             // By subtracting 97 from the ASCII value of any lowercase letter,
+                             // you effectively map the letters 'a' to 'z' to the range 0 to 25. This is because:
+                             // The ASCII value of 'a' is 97, so arr.charCodeAt(i) - 97 for 'a' is 97 - 97 = 0.
+                             // The ASCII value of 'b' is 98, so arr.charCodeAt(i) - 97 for 'b' is 98 - 97 = 1.
+                             //   And so on, until:
+                             // The ASCII value of 'z' is 122, so arr.charCodeAt(i) - 97 for 'z' is 122 - 97 = 25.
     }
     let sortedString = '';
     for (let i = 0; i < 26; i++) {
