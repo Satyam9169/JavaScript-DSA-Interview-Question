ab60e00ccc59f6528b5a8ef1b398a8fc120af0f1
Satyam Agrahari <satyamagrahari952@gmail.com>
2024-08-09 15:01:57 +0530

    added dry and run of anagram program

--- String/anagram.js
+++ String/anagram.js
@@ -32,3 +32,54 @@ console.log(anagram('silent', 'listen'))
 console.log(anagram("triangle", "integral"))
 console.log(anagram("apple", "pale"))
 console.log(anagram("Dormitory", "dirty room"))
+
+/*
+Dry and run of this code
+
+Initial Setup:
+str1 = 'silent'
+str2 = 'listen'
+map = new Map()
+Step-by-Step Execution:
+First loop (Building the map):
+
+Iteration 1: num = 's'
+'s' is not in the map, so add it with a count of 1.
+map = {'s' => 1}
+Iteration 2: num = 'i'
+'i' is not in the map, so add it with a count of 1.
+map = {'s' => 1, 'i' => 1}
+Iteration 3: num = 'l'
+'l' is not in the map, so add it with a count of 1.
+map = {'s' => 1, 'i' => 1, 'l' => 1}
+Iteration 4: num = 'e'
+'e' is not in the map, so add it with a count of 1.
+map = {'s' => 1, 'i' => 1, 'l' => 1, 'e' => 1}
+Iteration 5: num = 'n'
+'n' is not in the map, so add it with a count of 1.
+map = {'s' => 1, 'i' => 1, 'l' => 1, 'e' => 1, 'n' => 1}
+Iteration 6: num = 't'
+'t' is not in the map, so add it with a count of 1.
+map = {'s' => 1, 'i' => 1, 'l' => 1, 'e' => 1, 'n' => 1, 't' => 1}
+Second loop (Validating the second string):
+
+Iteration 1: num = 'l'
+'l' is in the map, so set its count to -1.
+map = {'s' => 1, 'i' => 1, 'l' => -1, 'e' => 1, 'n' => 1, 't' => 1}
+Iteration 2: num = 'i'
+'i' is in the map, so set its count to -1.
+map = {'s' => 1, 'i' => -1, 'l' => -1, 'e' => 1, 'n' => 1, 't' => 1}
+Iteration 3: num = 's'
+'s' is in the map, so set its count to -1.
+map = {'s' => -1, 'i' => -1, 'l' => -1, 'e' => 1, 'n' => 1, 't' => 1}
+Iteration 4: num = 't'
+'t' is in the map, so set its count to -1.
+map = {'s' => -1, 'i' => -1, 'l' => -1, 'e' => 1, 'n' => 1, 't' => -1}
+Iteration 5: num = 'e'
+'e' is in the map, so set its count to -1.
+map = {'s' => -1, 'i' => -1, 'l' => -1, 'e' => -1, 'n' => 1, 't' => -1}
+Iteration 6: num = 'n'
+'n' is in the map, so set its count to -1.
+map = {'s' => -1, 'i' => -1, 'l' => -1, 'e' => -1, 'n' => -1, 't' => -1}
+Result: Since all characters in str2 were found in str1 and all corresponding counts have been decremented, the function returns true.
+*/
